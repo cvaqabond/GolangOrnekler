23ce44e71e1d1468e6f08e6011df851eaa83bb33
ixakblt <ixakblt@gmail.com>
2020-10-09 17:42:05 +0300

    mapler ve duzenleme

---
+++ Mapornek/mapler.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	yer := make(map[string]string) // Key ve Val string olan bir map oluşturduk
+	yer["isim"] = "Alice"          // isim keyine aliyi atadık
+	fmt.Println(yer["isim"])       // isim sorgusunun çıktısını istedik çıktı olarak Alice değerini aldık
+
+}
