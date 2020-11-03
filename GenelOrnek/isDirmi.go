0be3650f6f5e3933f3facaa641abfb0eb17da1e3
ibrahim AKBULUT <57018928+ixakblt@users.noreply.github.com>
2020-11-03 20:40:25 +0200

    Create isDirmi.go

---
+++ GenelOrnek/isDirmi.go
@@ -0,0 +1,20 @@
+package main
+
+//kütüphane import etme
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+func main() {
+	// klasörü okuyup dizin adında bir nesneye atadık
+	dizin, _ := ioutil.ReadDir(".")
+	for _, a := range dizin { // dizin içerisinde range ile gezdik bu pythondaki for i in mantığına benzemekte
+		if a.IsDir() { // eğer dizinse yapılacakalr
+			fmt.Println(a.Name(), "dizindir")
+		} else { // değilse yapılacakalr
+			fmt.Printf("%s dizin degil\n", a.Name())
+		}
+
+	}
+}
